5438283012f15c4ac58bba21d720dc037b5e5c9e
StefanMol <stefan.mol@outlook.com>
2019-11-24 15:12:07 +0100

    Grid Home page
    Eerste versie van de home page

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Templates render the content of your pages. 
+ * Templates render the content of your pages.
  * They contain the markup together with some control structures like loops or if-statements.
- * The `$page` variable always refers to the currently active page. 
+ * The `$page` variable always refers to the currently active page.
  * To fetch the content from each field we call the field name as a method on the `$page` object, e.g. `$page->title()`.
  * This example templates only echos the field values from the content file and doesn't need any special logic from a controller.
  * Snippets like the header, footer and intro contain markup used in multiple templates. They also help to keep templates clean.
